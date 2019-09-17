b082450feaef7fb073ffa0d2dd79e10c8e926269
timotillydo <44656173+timotillydo@users.noreply.github.com>
2019-09-17 16:17:52 +0100

    added article list and article card components with links ready for users, topics and single article pages

--- src/Components/ArticleCard.js
+++ src/Components/ArticleCard.js
@@ -1,12 +1,34 @@
 import React from "react";
+import "../styles/ArticleCard.css";
+import { Link } from "@reach/router";
 
 const ArticleCard = ({ title, body, votes, topic, author, created_at }) => {
   return (
     <div className="article-card">
       <article>
-        <h2>{title}</h2>
+        <header className="article-header">
+          <div className="article-provenance">
+            <h5>
+              <Link to="/topics/:slug">from: {topic}</Link>
+            </h5>
+            <h5>
+              <Link to="/users/:user_id">written by: {author}</Link>
+            </h5>
+          </div>
+          <Link to="/articles/:article_id">
+            <h3>{title}</h3>
+          </Link>
+        </header>
         <section>{body}</section>
       </article>
+      <div className="article-actions">
+        <label>
+          respect:
+          <div className="votes">{votes}</div>
+          <button>Yeh</button>
+          <button>Nah</button>
+        </label>
+      </div>
     </div>
   );
 };
