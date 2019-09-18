a2161f7e1c3ad2ce241efc071e905248a0a87f2a
timotillydo <44656173+timotillydo@users.noreply.github.com>
2019-09-18 12:37:45 +0100

    refactored into functional component

--- src/Components/TopicList.js
+++ src/Components/TopicList.js
@@ -1,21 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 import Loading from "./Loading";
 import TopicCard from "./TopicCard";
 
-class TopicList extends Component {
-  state = {};
-  render() {
-    const { topics, isLoading } = this.props;
-    return isLoading ? (
-      <Loading />
-    ) : (
-      <div className="topic-list">
-        {topics.map(topic => {
-          return <TopicCard {...topic} key={topic.slug} />;
-        })}
-      </div>
-    );
-  }
-}
+const TopicList = ({ topics, isLoading }) => {
+  return isLoading ? (
+    <Loading />
+  ) : (
+    <div className="topic-list">
+      {topics.map(topic => {
+        return <TopicCard {...topic} key={topic.slug} />;
+      })}
+    </div>
+  );
+};
 
 export default TopicList;
