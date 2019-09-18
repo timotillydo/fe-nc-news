ffdc707c13dc4c5b0bcfe3b42177f724522d0222
timotillydo <44656173+timotillydo@users.noreply.github.com>
2019-09-18 12:37:09 +0100

    comment list added with request on mount for comments by id and comment card created to display mapped comments

---
+++ src/Components/CommentCard.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "@reach/router";
+
+const CommentCard = ({ author, body, created_at, votes }) => {
+  return (
+    <div className="comment-card">
+      <header className="comment-header">
+        <div className="comment-provenance">
+          <h5>
+            <Link to="/users/:user_id">written by: {author}</Link>
+          </h5>
+          <h5>{created_at}</h5>
+        </div>
+      </header>
+      <section>{body}</section>
+      <div className="comment-actions">
+        <label>
+          respect:
+          <div className="votes">{votes}</div>
+          <button>Yeh</button>
+          <button>Nah</button>
+        </label>
+      </div>
+    </div>
+  );
+};
+
+export default CommentCard;
---
+++ src/Components/CommentList.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import "../styles/CommentList.css";
+import * as api from "../api";
+import CommentCard from "./CommentCard";
+import Loading from "./Loading";
+
+class CommentList extends Component {
+  state = {
+    comments: [],
+    isLoading: true
+  };
+
+  componentDidMount = () => {
+    this.fetchComments();
+  };
+
+  fetchComments = () => {
+    const { article_id } = this.props;
+    api
+      .getComments(article_id)
+      .then(comments => {
+        this.setState(currentState => {
+          return { comments, isLoading: false };
+        });
+      })
+      .catch(err => console.dir(err));
+  };
+
+  render() {
+    const { comments, isLoading } = this.state;
+    return isLoading ? (
+      <Loading />
+    ) : (
+      <div className="comment-list">
+        {comments.map(comment => {
+          return <CommentCard {...comment} key={comment.comment_id} />;
+        })}
+      </div>
+    );
+  }
+}
+
+export default CommentList;
