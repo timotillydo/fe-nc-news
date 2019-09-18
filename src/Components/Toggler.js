f60e14b8bf69d58cdb7952edbd6ee24b02f123f3
timotillydo <44656173+timotillydo@users.noreply.github.com>
2019-09-18 09:21:02 +0100

    added get topics, users and post topics reqs

--- src/Components/Toggler.js
+++ src/Components/Toggler.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import { Component } from "react";
 
 class Toggler extends Component {
   state = { show: false };
--- src/api.js
+++ src/api.js
@@ -4,7 +4,23 @@ const request = axios.create({
   baseURL: "https://timotillydo-nc-news.herokuapp.com/api"
 });
 
+// GET REQs
 export const getArticles = async () => {
   const { data } = await request.get("/articles");
   return data.articles;
+};
+export const getTopics = async () => {
+  const { data } = await request.get("/topics");
+  return data.topics;
+};
+export const getUsers = async () => {
+  const { data } = await request.get("/users");
+  return data.users;
+};
+
+//POST REQs
+
+export const postTopic = async (slug, description) => {
+  const { data } = await request.post("/topics", { slug, description });
+  return data.topic;
 };
