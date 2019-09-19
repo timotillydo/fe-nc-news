21b26a813adebc71cc61f321fa17c554f6fdf4dc
timotillydo <44656173+timotillydo@users.noreply.github.com>
2019-09-19 11:12:39 +0100

    user card and list with styles

---
+++ src/Components/Cards/UserCard.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "../../styles/User.css";
+import { Link } from "@reach/router";
+
+const UserCard = ({ username, avatar_url, name }) => {
+  return (
+    <div className="card user">
+      {avatar_url && (
+        <img src={`${avatar_url}`} alt="user's avatar" className="avatar-img" />
+      )}
+      <ul className="user-info">
+        <li>
+          <h2>
+            <Link
+              to="/users/:user_id"
+              className="username"
+            >{`@${username}`}</Link>
+          </h2>
+        </li>
+        <li className="name">
+          <h3>{name}</h3>
+        </li>
+        <div>
+          <li className="artilcle-count">article_count</li>
+          <li className="best-article">highest_respect_article</li>
+        </div>
+      </ul>
+    </div>
+  );
+};
+
+export default UserCard;
