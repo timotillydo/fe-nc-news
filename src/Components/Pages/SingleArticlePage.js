41d9adb79142debbe7095d8ea1e2dd5ad70ac5cf
timotillydo <44656173+timotillydo@users.noreply.github.com>
2019-09-23 10:43:03 +0100

    remove article from single article page and redirect to home on delete

--- src/Components/Pages/SingleArticlePage.js
+++ src/Components/Pages/SingleArticlePage.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import * as api from "../../api";
-import ArticleCard from "../Cards/ArticleCard";
+import { Link, navigate } from "@reach/router";
 import CommentList from "../Lists/CommentList";
 import Loading from "../Loading";
 import DisplayError from "../DisplayError";
+import Voting from "../Voting";
 
 class SingleArticlePage extends Component {
   state = { article: {}, isLoading: true, err: null };
@@ -32,16 +33,55 @@ class SingleArticlePage extends Component {
       });
   };
 
+  handleOnClick = e => {
+    const article_id = e.target.value;
+    this.removeSingleArticle(article_id);
+  };
+
+  removeSingleArticle = article_id => {
+    api.deleteArticle(article_id).then(() => {
+      navigate("/");
+    });
+  };
+
   render() {
-    const { article, isLoading, err } = this.state;
-    const { article_id, loggedInUser } = this.props;
+    const {
+      article: { article_id, title, body, votes, topic, author, created_at },
+      isLoading,
+      err
+    } = this.state;
+    const { loggedInUser } = this.props;
     return isLoading ? (
       <Loading />
     ) : err ? (
       <DisplayError err={err} />
     ) : (
       <>
-        <ArticleCard {...article} loggedInUser={loggedInUser} />
+        <article>
+          <header className="article-header">
+            <div className="article-provenance">
+              <h5>
+                <Link to={`/topics/${topic}`}>from: {topic}</Link>
+              </h5>
+              <h5>
+                <Link to={`/users/${author}`}>written by: @{author}</Link>
+              </h5>
+              <h5>published: {created_at || "Date not provided."}</h5>
+            </div>
+            <Link to={`/articles/${article_id}`}>
+              <h3>{title}</h3>
+            </Link>
+          </header>
+          <section>{body}</section>
+        </article>
+        <div className="article-actions">
+          <Voting votes={votes} article_id={article_id} />
+          {loggedInUser === author && (
+            <button value={article_id} onClick={this.handleOnClick}>
+              Delete Article
+            </button>
+          )}
+        </div>
         <CommentList article_id={article_id} loggedInUser={loggedInUser} />
       </>
     );
