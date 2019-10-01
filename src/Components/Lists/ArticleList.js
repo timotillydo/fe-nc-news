72b88dd2ae14b454641dfea0dd11481bd9e1aefc
timotillydo <44656173+timotillydo@users.noreply.github.com>
2019-10-01 19:18:36 +0100

    replaced pages for limit when requesting

--- src/Components/Lists/ArticleList.js
+++ src/Components/Lists/ArticleList.js
@@ -5,7 +5,7 @@ import ArticleCard from "../Cards/ArticleCard";
 import Loading from "../Loading";
 import ArticleActionBar from "../ArticleActionBar";
 import DisplayError from "../DisplayError";
-import throttle from "lodash.throttle";
+import debounce from "lodash.debounce";
 
 class ArticleList extends Component {
   state = {
@@ -15,7 +15,7 @@ class ArticleList extends Component {
     orderBy: null,
     err: null,
     fetchMore: true,
-    page: 1
+    limit: 10
   };
 
   componentDidMount = () => {
@@ -27,7 +27,7 @@ class ArticleList extends Component {
     if (prevProps.topic !== this.props.topic) this.fetchArticles();
     if (prevState.sortBy !== this.state.sortBy) this.fetchArticles();
     if (prevState.orderBy !== this.state.orderBy) this.fetchArticles();
-    if (this.state.fetchMore && prevState.page !== this.state.page) {
+    if (this.state.fetchMore && prevState.limit !== this.state.limit) {
       this.fetchArticles();
     }
   };
@@ -36,36 +36,39 @@ class ArticleList extends Component {
     window.addEventListener("scroll", this.handleScroll);
   };
 
-  handleScroll = throttle(() => {
+  handleScroll = debounce(() => {
     if (
       window.innerHeight + document.documentElement.scrollTop ===
       document.documentElement.offsetHeight
     ) {
-      console.log("scrolled");
       this.setState(currentState => {
         return {
-          page: currentState.page + 1
+          limit: currentState.limit + 10
         };
       });
     }
-  }, 1500);
+  }, 2000);
 
   fetchArticles = () => {
-    const { sortBy, orderBy, page } = this.state;
+    const { sortBy, orderBy, limit } = this.state;
     const { topic, author } = this.props;
     this.setState({ isLoading: true });
     api
-      .getArticles(sortBy, orderBy, topic, author, page)
+      .getArticles(sortBy, orderBy, topic, author, limit)
       .then(({ articles, total_count }) => {
-        const getMoreArticles = total_count < page * 10;
-        if (getMoreArticles) {
+        const noMoreFetching = total_count < limit;
+        if (noMoreFetching) {
           this.setState({ fetchMore: false, isLoading: false });
         }
         this.setState(currentState => {
           return {
-            articles: [...currentState.articles, ...articles],
+            articles: [...articles],
             isLoading: false,
-            err: null
+            sortBy: currentState.sortBy,
+            orderBy: currentState.orderBy,
+            err: null,
+            fetchMore: currentState.fetchMore,
+            limit: currentState.limit
           };
         });
       })
@@ -81,7 +84,15 @@ class ArticleList extends Component {
   };
 
   changeSortBy = (sort, order) => {
-    this.setState({ sortBy: sort, orderBy: order });
+    this.setState({
+      articles: [],
+      sortBy: sort,
+      orderBy: order,
+      page: 1,
+      fetchMore: true,
+      isLoading: true,
+      err: null
+    });
   };
 
   removeArticle = article_id => {
